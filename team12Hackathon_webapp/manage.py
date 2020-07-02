b2efa0e84d349f2cb6d76f51b25ba94000da15a6
tomi <tomislavdz98@gmail.com>
2020-07-02 15:37:59 +0200

    django app

--- team12Hackathon_webapp/manage.py
+++ team12Hackathon_webapp/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'team12Hackathon.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'team12Hackathon_webapp.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- team12Hackathon_webapp/team12Hackathon_webapp/wsgi.py
+++ team12Hackathon_webapp/team12Hackathon_webapp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for team12Hackathon project.
+WSGI config for team12Hackathon_webapp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'team12Hackathon.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'team12Hackathon_webapp.settings')
 
 application = get_wsgi_application()
