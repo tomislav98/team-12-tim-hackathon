dc0566bfd226da67df2a45ff9c937927fe40f387
Tazio <andreatgiusti@gmail.com>
2020-07-03 02:54:52 +0200

    funzionano i cestini del menga

--- team12Hackathon_webapp/Hackathon_app/urls.py
+++ team12Hackathon_webapp/Hackathon_app/urls.py
@@ -5,5 +5,6 @@ from Hackathon_app import views
 urlpatterns = [
     path('', views.homepage, name='home'),
     path('index/', views.index, name='index'),
+    path('map/', views.map_view, name='map'),
 
     ]
--- team12Hackathon_webapp/Hackathon_app/views.py
+++ team12Hackathon_webapp/Hackathon_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+import json
 # Create your views here.
 
 def homepage(request):
@@ -7,5 +8,21 @@ def homepage(request):
     return render(request, 'base.html',{})
 
 def index(request):
-
     return render(request, 'index.html',{})
+
+# this function renders map template with openlayers.
+def map_view(request):
+    print(request.GET)
+    if request.method == 'GET' and request.GET: # if get request has data, it's a different call.
+        data = {
+            'bins' : [
+                {
+                    'id' : 1,
+                    'longitude' : 45.449903,
+                    'latitude' : 11.890073
+                }
+            ]
+        }
+        print("risposta json")
+        return JsonResponse(data)
+    return render(request, 'map.html',{})
