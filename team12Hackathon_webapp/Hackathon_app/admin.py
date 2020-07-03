aa73e2dd6fee004e841b9cf07016a61f50578fe7
kaido98 <tomislavdz98@gmail.com>
2020-07-03 12:00:16 +0200

    models update

--- team12Hackathon_webapp/Hackathon_app/admin.py
+++ team12Hackathon_webapp/Hackathon_app/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from Hackathon_app.models import User
+from Hackathon_app.models import User, KindAction, KindMission
 # Register your models here.
 admin.site.register(User)
+admin.site.register(KindAction)
+admin.site.register(KindMission)
