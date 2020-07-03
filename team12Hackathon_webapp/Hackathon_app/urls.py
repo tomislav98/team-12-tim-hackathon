315391b7fdf2188d41015926c658687eacc84b27
tomi <tomislavdz98@gmail.com>
2020-07-03 10:10:42 +0200

    Auto stash before merge of "develop" and "origin/develop"

--- team12Hackathon_webapp/Hackathon_app/urls.py
+++ team12Hackathon_webapp/Hackathon_app/urls.py
@@ -5,6 +5,7 @@ from Hackathon_app import views
 urlpatterns = [
     path('', views.homepage, name='home'),
     path('index/', views.index, name='index'),
+    path('modal/', views.modal, name='modal'),
     path('map/', views.map_view, name='map'),
 
     ]
