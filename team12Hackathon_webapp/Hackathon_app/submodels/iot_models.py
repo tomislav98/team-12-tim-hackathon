623c2085495ee67e3939060c6ed9b3696ea6deaa
Tazio <andreatgiusti@gmail.com>
2020-07-03 15:00:24 +0200

    migrations icon feature

--- team12Hackathon_webapp/Hackathon_app/submodels/iot_models.py
+++ team12Hackathon_webapp/Hackathon_app/submodels/iot_models.py
@@ -1,5 +1,14 @@
 from django.db import models
 from datetime import datetime
+
+class KindIconFeature(models.Model):
+    Id = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=200, null=False)
+    DisplayName = models.CharField(max_length=200)
+    Description = models.CharField(max_length=2000)
+    Base64 = models.BinaryField(max_length=9999)
+
+
 class KindGenericDevice(models.Model):
 
     Id = models.AutoField(primary_key=True)
@@ -16,3 +25,4 @@ class BinDevice(KindGenericDevice):
     LatitudePosition = models.FloatField(max_length=2000, null=False)
     Zone = models.CharField(max_length=2000,null=False)
     LastUpdate = models.DateTimeField(auto_created=True)
+    Icon = models.ForeignKey(KindIconFeature, on_delete=models.CASCADE)
