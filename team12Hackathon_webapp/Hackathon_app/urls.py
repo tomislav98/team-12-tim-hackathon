29af281f9795346d5ea5fc5767aaef43c7dc0685
kaido98 <tomislavdz98@gmail.com>
2020-07-04 20:50:23 +0200

    dashboard url fix

--- team12Hackathon_webapp/Hackathon_app/urls.py
+++ team12Hackathon_webapp/Hackathon_app/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from Hackathon_app import views
 
 urlpatterns = [
-    path('', views.homepage, name='home'),
+    path('', views.dashboard, name='dashboard'),
     path('map/', views.map_view, name='map'),
     path('missions/', views.missions, name='missions'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('map/bins', views.bins_map, name='bins'),
-    path('map/reports', views.report_map, name='reports'),
+path('map/reports', views.report_map, name='reports'),
     ]
