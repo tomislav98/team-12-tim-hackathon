a6edb0d44d9643de00f8a37a50d0e211b314e4e1
Tazio <andreatgiusti@gmail.com>
2020-07-04 22:40:11 +0200

    fix encrypt password

--- team12Hackathon_webapp/Hackathon_app/admin.py
+++ team12Hackathon_webapp/Hackathon_app/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
 from Hackathon_app.models import User, KindAction, KindMission, BinDevice, KindIconFeature, ReportMap
 
 # Register your models here.
-admin.site.register(User)
+# admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(KindAction)
 admin.site.register(KindMission)
 admin.site.register(BinDevice)
